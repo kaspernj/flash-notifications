63ae73b160fe2e80e792cca77a4a2e99c4b8436c
kaspernj <kasper@diestoeckels.de>
2026-03-10 07:02:13 +0000

    Upgrade ESLint to 10.0.3

---
+++ eslint.config.js
@@ -0,0 +1,40 @@
+const js = require("@eslint/js")
+const globals = require("globals")
+
+/**
+ * @param {Record<string, boolean | "readonly" | "writable" | "off">} input
+ * @returns {Record<string, boolean | "readonly" | "writable" | "off">}
+ */
+function sanitizeGlobals(input) {
+  return Object.fromEntries(
+    Object.entries(input).map(([key, value]) => [key.trim(), value])
+  )
+}
+
+module.exports = [
+  {
+    ignores: ["build"]
+  },
+  js.configs.recommended,
+  {
+    files: ["src/**/*.{js,jsx}"],
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true
+        }
+      },
+      globals: {
+        ...sanitizeGlobals(globals.browser),
+        ...sanitizeGlobals(globals.node)
+      }
+    },
+    rules: {
+      "eqeqeq": "off",
+      "no-return-assign": "off",
+      "no-unused-vars": ["error", {varsIgnorePattern: "^React$"}]
+    }
+  }
+]
